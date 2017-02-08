ceb2651b672d99fc43a03531bfedb704d0b96930
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-02-09 04:46:44 +0800

    Completed Minimum Requirements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,91 @@
-console.log('Hello World');
+(function() {
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lettersArr = letters.split("");
+    var chosenWord, chosenWordArr, guessedWordArr, numOfMisses, numOfCorrect;
+
+    resetGame();
+
+    function guessLetter() {
+        var letter = $(this).attr("data-letter");
+        $(this).prop("disabled", true);
+        $(this).removeClass("btn-info");
+
+        var found = false;
+        for (var i = 0; i < chosenWordArr.length; i++) {
+            if (chosenWordArr[i] === letter) {
+                found = true;
+                numOfCorrect++;
+                guessedWordArr[i] = letter;
+            }
+        }
+
+        if (found) {
+            $(this).addClass("btn-success");
+            correctLetter();
+        } else {
+            $(this).addClass("btn-danger");
+            wrongLetter();
+        }
+    }
+
+    function correctLetter() {
+        $("#characters > .character").each(function(i) {
+            $(this).text(guessedWordArr[i]);
+        });
+
+        if (numOfCorrect === chosenWord.length) finishGame(true);
+    }
+
+    function wrongLetter() {
+        numOfMisses--;
+        $("#misses").text(numOfMisses);
+
+        if (numOfMisses === 0) finishGame(false);
+    }
+
+    function finishGame(win) {
+        $(".alert-row").show();
+
+        if (win) $("#win").show();
+        else $("#lose").show();
+    }
+
+    function setWord() {
+        var words = ["GAME", "HANGMAN", "BITTITAN", "INTERVIEW", "JAVASCRIPT", "DOG", "CAT", "BALL"];
+        chosenWord = words[Math.floor(Math.random() * words.length)];
+        chosenWordArr = chosenWord.split("");
+
+        var characters = $("#characters");
+        guessedWordArr = [];
+        for (var i = 0; i < chosenWordArr.length; i++) {
+            characters.append('<div class="character">_</div>');
+            guessedWordArr.push("_");
+        }
+    }
+
+    function setupUI() {
+        var keypad = $("#keypad");
+        for (var i = 0; i < lettersArr.length; i++) {
+            keypad.append('<button class="btn btn-info letter-btn" data-letter=' + lettersArr[i] + '>' + lettersArr[i] + '</button>');
+        }
+
+        $(".letter-btn").click(guessLetter);
+        $(".reset-btn").click(resetGame);
+    }
+
+    function resetGame() {
+        $("#keypad").empty();
+        $("#characters").empty();
+
+        numOfCorrect = 0;
+        numOfMisses = 9;
+        $("#misses").text(numOfMisses);
+
+        $(".alert-row").hide();
+        $("#win").hide();
+        $("#lose").hide();
+
+        setupUI();
+        setWord();
+    }
+})();
